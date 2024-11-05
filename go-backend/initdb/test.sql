dab9b9a34667f713715984e37e828586e008e677
Jesper Bertijn <bert0018@hz.nl>
2024-11-05 13:39:10 +0100

    Added MariaDB Compose

---
+++ go-backend/initdb/test.sql
@@ -0,0 +1,8 @@
+CREATE TABLE test_table
+(
+    id      INT AUTO_INCREMENT PRIMARY KEY,
+    message VARCHAR(255) NOT NULL
+);
+
+INSERT INTO test_table (message)
+VALUES ('Initialization script executed successfully.');
