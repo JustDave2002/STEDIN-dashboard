295c54d754901d7ac98d388263fdff8728b8b2ea
Jesper Bertijn <bert0018@hz.nl>
2024-11-25 17:05:58 +0100

    Updated gitignore

--- kubernetes/remove_port_forwarding.ps1
+++
@@ -1,8 +0,0 @@
-# PowerShell Script to Remove Port Forwarding in WSL
-# Run this script in an elevated PowerShell session
-netsh interface portproxy delete v4tov4 listenport=6443 listenaddress=0.0.0.0
-netsh interface portproxy delete v4tov4 listenport=2379 listenaddress=0.0.0.0
-netsh interface portproxy delete v4tov4 listenport=2380 listenaddress=0.0.0.0
-netsh interface portproxy delete v4tov4 listenport=10250 listenaddress=0.0.0.0
-netsh interface portproxy delete v4tov4 listenport=10251 listenaddress=0.0.0.0
-netsh interface portproxy delete v4tov4 listenport=10252 listenaddress=0.0.0.0
